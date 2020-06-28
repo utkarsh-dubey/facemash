562284a74391efdd612d43e2d70badc3202e20d9
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-06-29 00:45:53 +0530

    dekhte hai

--- face/settings.py
+++ face/settings.py
@@ -120,15 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
     
-]
+# ]
 # STATICFILES_FINDERS = (
 # 'django.contrib.staticfiles.finders.FileSystemFinder',
 # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATIC_ROOT=''
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
