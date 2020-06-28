5fb4adccb664456dbb61ce0b590c1e72c2407cb7
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-06-27 18:57:34 -0600

    added random and images

--- face/app1/views.py
+++ face/app1/views.py
@@ -3,8 +3,15 @@ from random import randint
 
 
 def index(request):
-	# a=randint(1,5)
-	# a="/"+str(a)+".jpg"
-
-	# args={"a":a}
-	return render(request,'home.html')
+	
+	a = randint(1, 56)
+	b = randint(1, 56)
+	while a==b:
+		b - randint(1, 56)
+	filepath = "/static/"+str(a)+".jpg"
+	filepath2 = "/static/"+str(b)+".jpg"
+	args = {
+		"a":filepath,
+		"b":filepath2
+	}
+	return render(request,'home.html',args)
