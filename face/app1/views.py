c457883a29e0871b4865d608588e9d85e8f70bea
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-06-28 13:56:14 +0530

    winner number

--- face/app1/views.py
+++ face/app1/views.py
@@ -4,19 +4,10 @@ from random import randint
 
 def index(request):
 	
-	winner ='nope'
-	loser=''
-	if 'first' in request.POST:
-		winner='first'
-		loser='second'
-	elif 'second' in request.POST:
-		winner='second'
-		loser='first'
-
-	print(winner)
-	print(loser)
+	
 	a = randint(1, 94)
 	b = randint(1, 94)
+	print(a)
 	while a==b:
 		b - randint(1, 56)
 	filepath = "/static/"+str(a)+".jpg"
@@ -25,4 +16,18 @@ def index(request):
 		"a":filepath,
 		"b":filepath2
 	}
+	winner =''
+	loser=''
+	if 'first' in request.POST:
+		winner='first'
+		winner_number=request.POST['first']
+		winner_number=winner_number[winner_number.index('c')+2:winner_number.index('.')]
+		loser='second'
+	elif 'second' in request.POST:
+		winner='second'
+		winner_number=request.POST['second']
+		winner_number=winner_number[winner_number.index('c')+2:winner_number.index('.')]
+		loser='first'
+
+		
 	return render(request,'home.html',args)
