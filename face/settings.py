67108d7b2f2c19ea0fc84780848e24fea0d345bb
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-06-29 01:36:53 +0530

    okay

--- face/settings.py
+++ face/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +139,4 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+django_heroku.settings(locals())
